ecdd8ee197cbe9ec3c2581109736fa89535880e8
fu20232050 <481228484@qq.com>
2025-10-31 10:28:19 +0800

    12.5.9

--- task2/vite.config.js
+++ task2/vite.config.js
@@ -1,14 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // 部署在根路径时保持 '/'，若为子路径则改为 '/子路径/'
   plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
+  },
+  build: {
+    outDir: 'dist'  // 默认就是dist，确认没有被修改
   }
 })
