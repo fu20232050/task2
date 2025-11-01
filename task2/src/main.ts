aec76212e261994e3d2099e4f0f6cc58337c0143
fu20232050 <481228484@qq.com>
2025-11-01 13:19:14 +0800

    13.6.1

--- task2/src/main.ts
+++ task2/src/main.ts
@@ -1,22 +1,16 @@
+// main.ts
 import { createApp } from 'vue'
-import { createPinia } from 'pinia' // 导入 Pinia 创建函数
-//import './style.css'
-import './assets/style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router' 
-import 'nprogress/nprogress.css'
+import router from './router'
+
+// 使用默认导入方式
 import { inject } from '@vercel/analytics'
+
+// 注入分析
 inject()
 
-// 1. 创建 Pinia 实例
-const pinia = createPinia()
-
-// 2. 创建 Vue 应用
 const app = createApp(App)
-
-// 3. 注册 Pinia 和 Router 到应用中
-app.use(pinia)
+app.use(createPinia())
 app.use(router)
-
-// 4. 挂载应用
 app.mount('#app')
